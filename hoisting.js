3aa3f39989e52853d9723239f24cbd7696ef09f0
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-17 14:43:22 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -12,12 +12,13 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
+    thisIsCrazy();
   }
 }
+
+
 
 function sayMyName() {
   // fix the code in here:
